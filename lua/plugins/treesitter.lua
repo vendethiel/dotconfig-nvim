a1ac1c04e1a0d183f173e4dd26d2c318e885612b
vendethiel <vendethiel@hotmail.fr>
2025-11-28 07:54:05 +0100

    ts/js

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
   build = ":TSUpdate",
   config = function ()
     require("nvim-treesitter.configs").setup {
-      ensure_installed = { "c", "lua", "scala", },
+      ensure_installed = { "c", "lua", "scala", "javascript", "typescript", },
 
       auto_install = true,
 
